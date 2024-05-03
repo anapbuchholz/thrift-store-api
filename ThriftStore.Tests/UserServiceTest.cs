56bce020884d72a3108863e1fa07ca4e3e76687b
anapbuchholz@gmail.com <anapbuchholz@gmail.com>
2024-05-03 10:33:28 -0300

    Implementing unit test

--- ThriftStore.Tests/UserServiceTest.cs
+++ ThriftStore.Tests/UserServiceTest.cs
@@ -11,24 +11,34 @@ namespace ThriftStore.Tests
         private readonly IUserService _userService;
         private readonly IAuthenticationRepository _authenticationRepository;
         private readonly Fixture _fixture;
+        private readonly UserDto userDto;
+        private readonly string userEmail;
+        private readonly string userPassword;
 
         public UserServiceTest()
         {
             _authenticationRepository = Substitute.For<IAuthenticationRepository>();
             _userService = new UserService(_authenticationRepository);
             _fixture = new Fixture();
+            userDto = _fixture.Create<UserDto>();
+            userEmail = userDto.Email = "person@email.com";
+            userPassword = userDto.Password = "password";
         }
 
         [TestMethod]
         public async Task RegisterUser_WhenUserAccessFirstTime_ShouldReturnPasswordAndEmail()
         {
-            var userDto = _fixture.Create<UserDto>();
-            var userEmail = userDto.Email = "person@email.com";
-            var userPassword = userDto.Password = "password";
-
             await _userService.RegisterUser(userDto);
 
             await _authenticationRepository.Received(1).RegisterUserAsync(userEmail, userPassword);
+        }
+
+        [TestMethod]
+        public async Task AuthorizeUser_WhenAccessing_Should()
+        {
+            await _userService.AuthorizeUser(userEmail, userPassword);
+
+            await _authenticationRepository.Received(1).AuthorizeUser(userEmail, userPassword);
         }
     }
 }
